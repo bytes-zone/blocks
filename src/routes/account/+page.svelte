890d3ba89901549f23db3679737ce94f238f096b
Brian Hicks <brian@brianthicks.com>
2025-08-07 11:43:13 -0500

    add a page to edit the profile

---
+++ src/routes/account/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import ProfileEditor from '$lib/components/ProfileEditor.svelte';
+  import { Account } from '$lib/schema';
+  import { AccountCoState } from 'jazz-tools/svelte';
+
+  const account = new AccountCoState(Account, {
+    resolve: {
+      profile: true,
+    },
+  });
+
+  let profile = $derived(account.current?.profile);
+</script>
+
+{#if profile}
+  <ProfileEditor {profile} />
+{:else}
+  <p>No profile found. Are you logged in?</p>
+{/if}
