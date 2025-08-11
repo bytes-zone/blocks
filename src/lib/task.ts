c7ff9da5413434c73cb34881e34479197337e4ae
Brian Hicks <brian@brianthicks.com>
2025-08-11 06:38:15 -0500

    use a proper parser

--- src/lib/task.ts
+++ src/lib/task.ts
@@ -1,4 +1,5 @@
 import { co, z } from 'jazz-tools';
+import * as chrono from 'chrono-node';
 
 export const Task = co.map({
   title: co.plainText(),
@@ -21,28 +22,49 @@ export const Task = co.map({
 
 export type Task = co.loaded<typeof Task>;
 
-const blocksRe = /\s?&(\d+)/g;
+const blocksRe = /\s?\bblocks:(\d+)/g;
 
-const dueRe = /\s?!(\d{4}-\d{2}-\d{2})/g;
+const dueRe = /(\s?\bdue:)(\w+)/g;
 
-const waitRe = /\s?>(\d{4}-\d{2}-\d{2})/g;
+const waitRe = /(\s?\bwait:)(\w+)/g;
 
 export function parseTask(
   input: string,
 ): Pick<Task, 'blocks' | 'due' | 'wait'> & { title: string } {
-  const rawBlocks = blocksRe.exec(input)?.[1];
-  const blocks = rawBlocks ? parseInt(rawBlocks, 10) : 0;
+  let title = input;
 
-  const rawDue = dueRe.exec(input)?.[1];
-  const due = rawDue ? new Date(rawDue) : undefined;
+  const rawBlocks = blocksRe.exec(title)?.[1];
+  let blocks = 0;
+  if (rawBlocks) {
+    blocks = parseInt(rawBlocks, 10);
+    title = title.replace(blocksRe, '');
+  }
 
-  const rawWait = waitRe.exec(input)?.[1];
-  const wait = rawWait ? new Date(rawWait) : undefined;
+  let due = undefined;
+  due: for (const match of title.matchAll(dueRe)) {
+    const dateStart = match.index + match[0].length - match[2].length;
+    for (const date of chrono.parse(title)) {
+      if (date.index === dateStart) {
+        due = date.start.date();
+        title = title.replace(`${match[1]}${date.text}`, '');
 
-  return {
-    title: input.replaceAll(blocksRe, '').replaceAll(dueRe, '').replaceAll(waitRe, '').trim(),
-    blocks,
-    due,
-    wait,
-  };
+        break due;
+      }
+    }
+  }
+
+  let wait = undefined;
+  wait: for (const match of title.matchAll(waitRe)) {
+    const dateStart = match.index + match[0].length - match[2].length;
+    for (const date of chrono.parse(title)) {
+      if (date.index === dateStart) {
+        wait = date.start.date();
+        title = title.replace(`${match[1]}${date.text}`, '');
+
+        break wait;
+      }
+    }
+  }
+
+  return { title, blocks, due, wait };
 }
