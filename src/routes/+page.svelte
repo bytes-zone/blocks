0fb5fa464f269681fe85e3a22563d5ad03e86b83
Brian Hicks <brian@brianthicks.com>
2025-08-14 22:23:59 -0500

    render main collection tasks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import TaskList from '$lib/components/TaskList.svelte';
+  import { Account } from '$lib/schema';
+  import { AccountCoState } from 'jazz-tools/svelte';
+
+  const account = new AccountCoState(Account, {
+    resolve: {
+      root: {
+        collection: {
+          $each: true,
+        },
+      },
+    },
+  });
+
+  const tasks = $derived(account.current?.root?.collection);
+</script>
+
+{#if tasks}
+  <TaskList list={tasks} />
+{/if}
