b04e8c7a848b1805fc2aa9a604378b803ec30e5a
Brian Hicks <brian@brianthicks.com>
2025-08-16 16:46:18 -0500

    add test for completing a task

---
+++ e2e/complete-task.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from '@playwright/test';
+
+test('you should be able to complete a task', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByPlaceholder('What do you need to do?').fill('Buy milk');
+  await page.getByRole('button', { name: 'Add' }).click();
+
+  await page.getByText('Inbox (1)').click();
+
+  const complete = page.getByLabel('Mark Buy milk complete');
+
+  await expect(complete).not.toBeChecked();
+  await complete.check();
+});
