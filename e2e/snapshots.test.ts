55ae5690c3f5e71d657b7aad31fd764520de76c6
Brian Hicks <brian@brianthicks.com>
2025-08-16 16:25:27 -0500

    add snapshot tests for aria layout

---
+++ e2e/snapshots.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from '@playwright/test';
+
+test('inbox page snapshot should be accessible', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByPlaceholder('What do you need to do?').fill('Buy milk');
+  await page.getByRole('button', { name: 'Add' }).click();
+
+  await page.getByPlaceholder('What do you need to do?').fill('Wash the car blocks:2');
+  await page.getByRole('button', { name: 'Add' }).click();
+
+  await page.getByText('Inbox (2)').click();
+
+  await expect(page.getByRole('main')).toMatchAriaSnapshot({ name: 'inbox.aria.yaml' });
+});
