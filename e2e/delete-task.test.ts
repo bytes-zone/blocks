53e5a199d860d63c039dd14822d3808ab3b80e23
Brian Hicks <brian@brianthicks.com>
2025-08-15 22:56:22 -0500

    test deleting a task

--- e2e/delete-task.test.ts
+++ e2e/delete-task.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('quick-adding a task should send it to the inbox', async ({ page }) => {
+test('you should be able to delete a task', async ({ page }) => {
   await page.goto('/');
 
   await page.getByPlaceholder('What do you need to do?').fill('Buy milk');
@@ -9,4 +9,7 @@ test('quick-adding a task should send it to the inbox', async ({ page }) => {
   await page.getByText('Inbox (1)').click();
 
   await expect(page.getByRole('cell', { name: 'Buy milk', exact: true })).toBeVisible();
+
+  await page.getByTitle('Delete Buy milk').click();
+  await expect(page.getByRole('cell', { name: 'Buy milk' })).not.toBeVisible();
 });
