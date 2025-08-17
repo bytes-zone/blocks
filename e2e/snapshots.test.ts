2c06f3d2536594b10aac80df3bcd31f1c8c33286
Brian Hicks <brian@brianthicks.com>
2025-08-16 23:20:44 -0500

    update snapshot

--- e2e/snapshots.test.ts
+++ e2e/snapshots.test.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test';
 test('inbox page snapshot should be accessible', async ({ page }) => {
   await page.goto('/');
 
-  await page.getByPlaceholder('What do you need to do?').fill('Buy milk');
+  await page.getByPlaceholder('What do you need to do?').fill('Buy milk due:8/20/25');
   await page.getByRole('button', { name: 'Add' }).click();
 
   await page.getByPlaceholder('What do you need to do?').fill('Wash the car blocks:2');
