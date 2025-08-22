e507ec458f81e86d09867011aa3536f8c41957d5
Brian Hicks <brian@brianthicks.com>
2025-08-22 17:22:54 -0500

    move to a login page and sidebar

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Account } from '$lib/schema/account';
+  import { AccountCoState } from 'jazz-tools/svelte';
+
+  const account = new AccountCoState(Account);
+</script>
+
+<button class="btn preset-outlined-primary-500" onclick={() => account.logOut()}>Log Out</button>
