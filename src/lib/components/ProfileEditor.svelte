9a643c721ccbfcd471cb73dc297290a945dae096
Brian Hicks <brian@brianthicks.com>
2025-08-22 07:28:28 -0500

    update name more frequently

--- src/lib/components/ProfileEditor.svelte
+++ src/lib/components/ProfileEditor.svelte
@@ -12,7 +12,7 @@
       placeholder="Kermit the Frog"
       type="text"
       value={profile.name}
-      onchange={(ev) => (profile.name = ev.currentTarget.value)}
+      oninput={(ev) => (profile.name = ev.currentTarget.value)}
     />
   </label>
 </form>
