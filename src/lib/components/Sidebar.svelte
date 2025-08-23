df334ee151814a8d4b230e36a438759c6c95d84c
Brian Hicks <brian@brianthicks.com>
2025-08-22 21:41:06 -0500

    add an inbox link

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,16 +1,52 @@
 <script lang="ts">
   import { Account } from '$lib/schema/account';
-  import { LogOut } from '@lucide/svelte';
+  import { Icon, Inbox, LogOut } from '@lucide/svelte';
   import { AccountCoState } from 'jazz-tools/svelte';
+  import { page } from '$app/state';
 
-  let account = new AccountCoState(Account, { resolve: { profile: true } });
+  let account = new AccountCoState(Account, {
+    resolve: { profile: true, root: { inbox: true } },
+  });
 </script>
+
+{#snippet link(
+  url: string,
+  label: string,
+  active: boolean,
+  icon: typeof Icon,
+  iconColor: string,
+  rhs: string = '',
+)}
+  {@const CurrentIcon = icon}
+  <li class="px-4 py-1" class:bg-primary-50-950={active}>
+    <a href={url} class="flex w-full cursor-pointer items-center justify-between">
+      <div class="flex gap-2">
+        <CurrentIcon class="w-5 {iconColor}" />
+        {label}
+      </div>
+      <div class="text-surface-700-300">{rhs}</div>
+    </a>
+  </li>
+{/snippet}
 
 <div
   class="flex min-h-screen w-xs flex-col border-r border-gray-200 bg-gray-50 dark:border-gray-800 dark:bg-gray-950"
 >
-  <div class="grow-1 p-4">
-    <p>Hey</p>
+  <div class="grow-1 py-4">
+    <nav aria-label="primary">
+      <ol class="flex flex-col gap-2">
+        {@render link(
+          '/inbox',
+          'Inbox',
+          page.route.id === '/inbox',
+          Inbox,
+          'text-primary-300-700',
+          account.current?.root?.inbox && account.current.root.inbox.length > 0
+            ? account.current.root.inbox.length.toString()
+            : '',
+        )}
+      </ol>
+    </nav>
   </div>
   <div
     class="flex shrink-1 items-center justify-between border-t border-gray-200 bg-gray-100 p-4 dark:border-gray-800 dark:bg-gray-900"
