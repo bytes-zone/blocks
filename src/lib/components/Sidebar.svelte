0bea73657fd9182c5d9c3942bdc87fd6d38aa747
Brian Hicks <brian@brianthicks.com>
2025-08-24 15:10:58 -0500

    allow adding new areas

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import { Account } from '$lib/schema/account';
-  import { CirclePlus, House, Icon, Inbox, LogOut } from '@lucide/svelte';
+  import { CirclePlus, House, Icon, Inbox, LogOut, MapPlus } from '@lucide/svelte';
   import { AccountCoState } from 'jazz-tools/svelte';
   import { page } from '$app/state';
   import QuickAdd from './QuickAdd.svelte';
+  import AreaForm from './AreaForm.svelte';
 
   let account = new AccountCoState(Account, {
     resolve: {
@@ -13,6 +14,7 @@
   });
 
   let quickAdd: HTMLDialogElement;
+  let newArea: HTMLDialogElement;
 </script>
 
 {#snippet link(
@@ -38,7 +40,7 @@
 <header
   class="flex min-h-screen w-xs flex-col border-r border-gray-200 bg-gray-50 dark:border-gray-800 dark:bg-gray-950"
 >
-  <div class="grow-1 py-4">
+  <div class="flex grow-1 flex-col gap-6 py-4">
     <nav aria-label="primary">
       <ol class="flex flex-col gap-2">
         {@render link('/', 'Home', page.route.id === '/', House, 'text-surface-300-700')}
@@ -61,7 +63,12 @@
   >
     <button class="btn" onclick={() => quickAdd.showModal()}>
       <CirclePlus class="w-4 text-success-500" />
-      <span>Add item</span>
+      <span class="sr-only">Add item</span>
+    </button>
+
+    <button class="btn" onclick={() => newArea.showModal()}>
+      <MapPlus class="w-4 text-success-500" />
+      <span class="sr-only">Add area</span>
     </button>
 
     {#if account.current && account.isAuthenticated}
@@ -86,6 +93,21 @@
         account.current.root.inbox.push(task);
       }
       quickAdd.close();
+    }}
+  />
+</dialog>
+
+<dialog
+  bind:this={newArea}
+  closedby="any"
+  class="top-1/2 left-1/2 -translate-1/2 rounded-container"
+>
+  <AreaForm
+    onsave={(area) => {
+      if (account?.current?.root?.areas) {
+        account.current.root.areas.push(area);
+      }
+      newArea.close();
     }}
   />
 </dialog>
