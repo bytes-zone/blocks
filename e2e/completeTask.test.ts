528a474f2909fcc891e04e436a00ea401a2bf4e4
Brian Hicks <brian@brianthicks.com>
2025-08-23 21:55:20 -0500

    fix up functional tests

--- e2e/completeTask.test.ts
+++ e2e/completeTask.test.ts
@@ -1,12 +1,10 @@
 import { expect, test } from '@playwright/test';
+import { goToInbox, quickAdd, start } from './utils';
 
 test('you should be able to complete a task', async ({ page }) => {
-  await page.goto('/');
-
-  await page.getByPlaceholder('What do you need to do?').fill('Buy milk');
-  await page.getByRole('button', { name: 'Add' }).click();
-
-  await page.getByText('Inbox (1)').click();
+  await start(page);
+  await quickAdd(page, 'Buy milk');
+  await goToInbox(page);
 
   const complete = page.getByRole('checkbox', { name: 'Mark Buy milk complete' });
 
