80e3d818ae4a3f6bd49389dbbceda9266ea93fca
Brian Hicks <brian@brianthicks.com>
2025-08-28 13:18:33 -0500

    add @custom-media processor

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,8 +1,16 @@
 import postcssJitProps from 'postcss-jit-props';
+import postcssCustomMedia from 'postcss-custom-media';
+import postcssGlobalData from '@csstools/postcss-global-data';
 import openProps from 'open-props';
 
 const config = {
-  plugins: [postcssJitProps(openProps)],
+  plugins: [
+    postcssGlobalData({
+      files: ['node_modules/open-props/media.min.css'],
+    }),
+    postcssCustomMedia(),
+    postcssJitProps(openProps),
+  ],
 };
 
 module.exports = config;
